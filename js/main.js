cb431693756c36b864297d5545b3bf54600b5fb3
tricshi <tric.shi@gmail.com>
2015-11-12 15:52:02 -0500

    copy reverse

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(window).load(function() {
 function launchTest(testId, testType) {
   var testCounter = currentTypeIndex + currentTestIndex * testTypes.length + 1;
   if (testCounter === 1) {
-    $('#timeoutDialog > div > div:nth-of-type(1) > span').text("This test will take about 5 minutes.  It would be best if this could be uninterrupted time.");
+    $('#timeoutDialog > div > div:nth-of-type(1) > span').text("This test will take about 5 minutes. Please make sure you have this much of uninterrupted time.");
     $('#timeoutDialog > div > div:nth-of-type(3) > span').text("Use your keyboard to navigate the following screens:");
     $('#timeoutDialog > div > div:nth-of-type(5) > span').html("&#x21E6;&#x21E7;&#x21E8;&#x21E9;: Move focus &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter/Return: Select");
   } else {
